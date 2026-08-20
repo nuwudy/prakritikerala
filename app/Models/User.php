6536a082c76a9989b0551dbb2fde07125981fa38
nuwudy <info@wensdoc.com>
2026-08-20 20:03:01 +0530

    Allow users to access Filament admin panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // You can restrict this to specific emails like: return $this->email === 'admin@prakritikerala.com';
+    }
 
     /**
      * The attributes that are mass assignable.
